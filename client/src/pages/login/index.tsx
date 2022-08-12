d2d3eea881d4e7e4578605ca87be6206e7a6e08e
Fadi Khouri <35076041+frkhouri@users.noreply.github.com>
2022-08-11 22:04:45 -0400

    Add login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,9 +1,25 @@
+import { Button, Container, CSSObject } from '@mantine/core';
+
 export default function LoginPage() {
   return (
-    <>
-      <div>
-        <a href={'http://localhost:3001/api/login'}>Log in</a>
-      </div>
-    </>
+    <Container sx={loginWrapper}>
+      <Button
+        component="a"
+        href={'http://localhost:3001/api/login'}
+        uppercase
+        sx={loginButton}
+      >
+        Log in
+      </Button>
+    </Container>
   );
 }
+
+const loginWrapper: CSSObject = {
+  display: 'flex',
+  minHeight: '100vh',
+};
+
+const loginButton: CSSObject = {
+  margin: 'auto',
+};
