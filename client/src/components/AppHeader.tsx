5514e58dbd615f4e012aedd1c386169ea4cdcec7
Fadi Khouri <35076041+frkhouri@users.noreply.github.com>
2022-08-19 07:31:45 -0400

    Adjust header

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -9,7 +9,7 @@ export function AppHeader() {
   return (
     <Header height={50} withBorder classNames={{ root: classes.root }}>
       <Group align="center" position="apart">
-        <Text size="xl" weight={500} transform="capitalize" p="xs" inline>
+        <Text size="xl" weight={500} transform="capitalize" p="xs">
           {location.state?.pageTitle ?? location.pathname.split('/')[1]}
         </Text>
         <ActionIcon variant="default">
@@ -24,7 +24,7 @@ const useStyles = createStyles((theme) => ({
   root: {
     backgroundColor:
       theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    margin: '0px -8px 15px -8px',
+    margin: '-8px -8px 15px -8px',
     padding: '0px 15px',
     top: '0px',
   },
